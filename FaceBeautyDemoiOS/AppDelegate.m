f3ec70b5a5a362e017a30d4ab97a5c971250bb70
tao <2631124232@qq.com>
2025-05-15 17:59:35 +0800

    更新4.1.0-beta版本

--- FaceBeautyDemoiOS/AppDelegate.m
+++ FaceBeautyDemoiOS/AppDelegate.m
@@ -17,7 +17,25 @@ NSString *isSDKInit = @"未初始化";
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[HomeViewController alloc] init]];
     [self.window makeKeyAndVisible];
 //    #error----在线鉴权密钥
-    [[FaceBeauty shareInstance] initFaceBeauty:@"YOUR_APP_ID" withDelegate:self];
+    
+    // 本地资源文件拷贝至沙盒路径
+    BOOL isResourceCopied = NO;
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FaceBeauty" ofType:@"bundle"];
+
+    NSArray *libraryPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    if (libraryPaths.count > 0) {
+        NSString *libraryDirectory = [libraryPaths lastObject];
+        NSString *sandboxPath = [libraryDirectory stringByAppendingPathComponent:@"FaceBeauty"];
+        isResourceCopied = [[FaceBeauty shareInstance] copyResourceBundle:bundlePath toSandbox:sandboxPath];
+    }
+
+    NSString *version = [[FaceBeauty shareInstance] getVersion];
+    NSLog(@"当前FaceBeauty版本 %@", version ?: @"");
+
+    # error 需要FaceBeauty appid，与包名应用名绑定，请联系商务获取
+    if (isResourceCopied) {
+        [[FaceBeauty shareInstance] initFaceBeauty:@"YOUR_APP_ID" withDelegate:nil];
+    }
     return YES;
 }
 
--- Podfile
+++ Podfile
@@ -6,5 +6,5 @@ target 'FaceBeautyDemoiOS' do
   use_frameworks!
 
   # Pods for FaceBeautyDemoiOS
-  pod 'FaceBeauty'
+  pod 'FaceBeauty','4.1.0-beta'
 end
